938eb1d9df022a763f11f8c84de11d61bceee631
tisrenac21 <tisrenac21@kakao.com>
2022-09-29 10:05:14 +0900

    RequestMapping Setting / HTML Setting.

--- src/main/java/jh/spring/mvc/controller/BoardController.java
+++ src/main/java/jh/spring/mvc/controller/BoardController.java
@@ -1,5 +1,23 @@
 package jh.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
 
+	@GetMapping("/list")
+	public String list() {
+		return "board/list";
+	}
+	
+	@GetMapping("/view")
+	public String view() {
+		return "board/view";
+	}
+	
+	@GetMapping("/write")
+	public String write() {
+		return "board/write";
+	}
 }
--- src/main/java/jh/spring/mvc/controller/MemberController.java
+++ src/main/java/jh/spring/mvc/controller/MemberController.java
@@ -1,5 +1,23 @@
 package jh.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
+	@GetMapping("/join")
+	public String join() {
+		return "join/join";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "join/login";
+	}
+	
+	@GetMapping("/myinfo")
+	public String myinfo() {
+		return "join/myinfo";
+	}
 
 }
