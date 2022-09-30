1f57df2db22b0903f01197f05a5565ce5dca5b9f
tisrenac21 <tisrenac21@kakao.com>
2022-09-30 16:56:03 +0900

    ROWMAPPER 사용

--- src/main/java/jh/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/jh/spring/mvc/dao/MemberDAOImpl.java
@@ -1,5 +1,7 @@
 package jh.spring.mvc.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collections;
 
 import javax.sql.DataSource;
@@ -24,8 +26,10 @@ public class MemberDAOImpl implements MemberDAO {
 	private SimpleJdbcInsert simpleInsert;
 	private NamedParameterJdbcTemplate jdbcNameTemplate;
 	
-	private RowMapper<MemberVO> memberMapper = BeanPropertyRowMapper.newInstance(MemberVO.class);
-	
+	/*
+	 * private RowMapper<MemberVO> memberMapper =
+	 * BeanPropertyRowMapper.newInstance(MemberVO.class);
+	 */	
 	public MemberDAOImpl(DataSource dataSource) {
 		simpleInsert = new SimpleJdbcInsert(dataSource).withTableName("member").usingColumns("member_id", "password", "member_name", "email");
 		
@@ -44,7 +48,25 @@ public class MemberDAOImpl implements MemberDAO {
 	public MemberVO selectOneMember() {
 		String sql = "select * from member where member_no = 1";
 		
-		return jdbcNameTemplate.queryForObject(sql, Collections.emptyMap(), memberMapper);
-	}
+		RowMapper<MemberVO> memberMapper = new MemberRowMapper();
+		
+	      return jdbcTemplate.queryForObject(sql, null, memberMapper);
+	      }
+	
+	// 콜백 메서드 정의: mapRow
+	private class MemberRowMapper implements RowMapper<MemberVO>{
+
+		@Override
+		public MemberVO mapRow(ResultSet rs, int num) throws SQLException {
+			MemberVO m = new MemberVO();
+			
+			m.setMemberId(rs.getString("memberId"));
+			m.setMemberName(rs.getString("memberName"));
+			m.setEmail(rs.getString("email"));
+			m.setRegDate(rs.getString("regDate"));
+		
+			return m;
+		}
+		}
 
 }
